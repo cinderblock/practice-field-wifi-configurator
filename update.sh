efba3ad227ab18fe33a00aaf94cdff52a29eaa40
Cameron Tacklind <cameron@tacklind.com>
2025-04-16 01:17:52 -0700

    Oops

--- update.sh
+++ update.sh
@@ -11,4 +11,4 @@ rsync -av --delete frontend/dist/ ../practice-field-wifi/internal/
 # Copy the public.html to the public directory
 cp frontend/src/public.html ../practice-field-wifi/public/index.html
 
-sudo systemctl restart --user practice-field-wifi.service
+systemctl restart --user practice-field-wifi.service
