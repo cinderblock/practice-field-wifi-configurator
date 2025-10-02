4df17a9ebba42c8c4a2312dbde9464f02a1a8f94
Cameron Tacklind <cameron@tacklind.com>
2025-10-02 00:46:36 -0700

    Add title to station status page

--- frontend/src/components/StationPage.tsx
+++ frontend/src/components/StationPage.tsx
@@ -1,6 +1,14 @@
+import { Typography } from '@mui/material';
 import { StationName } from '../../../src/types';
 import StationStatus from './StationStatus';
 
 export function StationPage({ station }: { station: StationName }) {
-  return <StationStatus full station={station} />;
+  return (
+    <>
+      <Typography variant="h4" gutterBottom>
+        Driver Station Status
+      </Typography>
+      <StationStatus full station={station} />
+    </>
+  );
 }
